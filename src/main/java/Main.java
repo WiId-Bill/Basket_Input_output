300a5a95d843b8ab80f1169340f88b69687016c5
Wild-Bill <spamy4@mail.ru>
2023-04-09 13:03:47 +0300

    add loa &&, save: json,txt,log

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,36 @@
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
         public class Main {
             static String[] products = {"Хлеб", "Яблоки", "Молоко", "Мороженое"};
             static int[] prices = {100, 200, 300, 450};
-            static  File saveFile = new File("basket.json");
-            public static void main(String[] args) throws FileNotFoundException {
+            static  File jsonFile = new File("basket.json");
+            static  File txtFile = new File("basket.txt");
+            static   boolean isLoad;
+            static String loadFile;
+            static    String loadFormat;
+
+            static    boolean isSave;
+            static    String saveFile;
+            static   String saveFormat;
+
+            static    boolean isLog;
+            static    String logFile;
+            public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
+                File xmlFile = new File("shop.xml");
+               XMLSetting(xmlFile);
 
                 Scanner scanner = new Scanner(System.in);
                 System.out.println("Список возможных товаров для покупки");
@@ -16,10 +38,8 @@ import java.util.Scanner;
             //    int[] prices = {100, 200, 300, 450};
             //    int[] productPrice = new int[4];
                 Basket basket = null;
-                if (saveFile.exists()) {
-                    basket = Basket.loadFromJSONFile(saveFile);
-                } else {  basket = new Basket(products, prices);
-                }
+
+                basket = getBasket(null);
 
                 for (int i = 0; i < products.length; i++) {
                     System.out.println((i + 1) + ". " + products[i] + "  " + prices[i] + " руб/шт");
@@ -32,7 +52,9 @@ import java.util.Scanner;
                     if ("end".equals(input)) {
                         System.out.println("Программа завершена!");
                         System.out.println("Ваша корзина:");
-                        log.exportAsCSV(new File("log.csv"));
+                        if(isLog){
+                        log.exportAsCSV(new File(logFile));
+                        }
                         break;
                     }
                     String[] parts = input.split(" ");
@@ -40,8 +62,56 @@ import java.util.Scanner;
                     int productCount = Integer.parseInt(parts[1]);
                     basket.addToCart(productNumber, productCount);
                     log.log(productNumber+1, productCount);
-                    basket.saveJSON(saveFile);
+                    if (isSave){
+                        switch (saveFormat){
+                            case "json": basket.saveJSON(jsonFile);
+                            break;
+                            case "txt":basket.saveTxt(txtFile);
+                            break;
+                        }
+                    }
+
                 }
                 basket.printCart();
+            }
+
+            private static Basket getBasket(Basket basket) {
+                if (isLoad && new File(loadFile).exists()) {
+                    switch (loadFormat) {
+                        case "json":
+                            basket = Basket.loadFromJSONFile(jsonFile);
+                            break;
+                        case "txt":
+                            basket = Basket.loadFromTxtFile(txtFile);
+                            break;
+                    }
+
+                } else {  basket = new Basket(products, prices);
+                    }
+                return basket;
+            }
+
+            public static void XMLSetting(File xmlFile) throws ParserConfigurationException, IOException, SAXException {
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                Document doc = builder.parse(xmlFile);
+
+                Element root = doc.getDocumentElement();
+//
+                Element loadSettings = (Element) root.getElementsByTagName("load").item(0);
+                Element saveSettings = (Element) root.getElementsByTagName("save").item(0);
+                Element logSettings = (Element) root.getElementsByTagName("log").item(0);
+
+                isLoad = Boolean.parseBoolean(loadSettings.getElementsByTagName("enabled").item(0).getTextContent());
+                loadFile = loadSettings.getElementsByTagName("fileName").item(0).getTextContent();
+                loadFormat = loadSettings.getElementsByTagName("format").item(0).getTextContent();
+
+                isSave = Boolean.parseBoolean(saveSettings.getElementsByTagName("enabled").item(0).getTextContent());
+                saveFile = saveSettings.getElementsByTagName("fileName").item(0).getTextContent();
+                saveFormat = saveSettings.getElementsByTagName("format").item(0).getTextContent();
+
+                isLog = Boolean.parseBoolean(logSettings.getElementsByTagName("enabled").item(0).getTextContent());
+                logFile = logSettings.getElementsByTagName("fileName").item(0).getTextContent();
+
             }
         }
